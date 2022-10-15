62c4e528c2047bff4c5fa2f5188257f5445fd013
Mohammed Nasser <31956229+MohmmedNasser@users.noreply.github.com>
2022-10-15 14:08:29 +0300

    fix issues in context menu

--- lec10/context_menu/assets/js/script.js
+++ lec10/context_menu/assets/js/script.js
@@ -5,6 +5,11 @@ let toggleColor = document.querySelector('#toggleColor'),
     customMenu = document.querySelector(".custom-menu");
 
 
+let getTheme = JSON.parse(localStorage.getItem('theme'));
+if(getTheme === 'dark') {
+    body.classList.add('dark');
+}
+
 // dark mode event
 toggleColor.onclick = () => {
     body.classList.toggle('dark');
@@ -17,32 +22,28 @@ toggleColor.onclick = () => {
     localStorage.setItem('theme', JSON.stringify(theme));
 }
 
-let getTheme = JSON.parse(localStorage.getItem('theme'));
-if(getTheme === 'dark') {
-    body.classList.toggle('dark');
-}
-
 
 // context menu
 window.oncontextmenu = (e) => {
     e.preventDefault();
-    e.stopPropagation();
-    let xPosition = e.offsetX,
-        yPosition = e.offsetY,
+    let xPosition = e.clientX,
+        yPosition = e.clientY,
         windowWidth = window.innerWidth,
         windowHeight = window.innerHeight,
-        menuWidth = customMenu.offsetWidth,
+        menuWidth = 200,
         menuHeight = customMenu.offsetHeight;
 
         // menu position
-        xPosition = xPosition > windowWidth - menuWidth ? windowWidth - menuWidth : xPosition;
-        yPosition = yPosition > windowHeight - menuHeight ? windowHeight - menuHeight  : yPosition;
+        xPosition = xPosition > windowWidth - menuWidth ? windowWidth - menuWidth - 20 : xPosition;
+        yPosition = yPosition > windowHeight - menuHeight ? windowHeight - menuHeight - 20  : yPosition;
 
         customMenu.style.left =  `${xPosition}px`;
         customMenu.style.top =  `${yPosition}px`;
         customMenu.style.display = 'block';
 
 }
+
+customMenu.onclick = (e) => e.stopPropagation();
 
 document.onclick = () => {
     customMenu.style.display = 'none';
